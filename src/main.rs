691cc51b07a6920007854c2140d15a4e395c44b1
yjmhyhc <yjmhyhc@126.com>
2024-12-16 14:55:36 -0500

    1. only showing 3000 characters at most in the output box, this is to avoid running out of memory. 2. refined some UI

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,31 @@ use cursive::theme::{BaseColor, Color};
 use crate::web::ReportEntry;
 use std::fs::File;
 use std::io::Write;
+use std::sync::{Arc, Mutex};
+#[macro_use]
+extern crate lazy_static;
 
+/*
+ this is a static variable to store the search result, it is static so it does not
+ need to be moved into the closures that we create, but can still be visited globally wide
+ */
+lazy_static!{
+    static ref GLOBAL_STRING: Arc<Mutex<String>> = Arc::new(Mutex::new(String::from("")));
+}
+
+///
+/// a main function that carries front end UI code
+///
 #[tokio::main]
 async fn main() {
-
+    // create the cursive front end struct
     let mut siv = Cursive::default();
 
     // the form to contain input information
     let form = LinearLayout::vertical()
         .child(
             LinearLayout::vertical()
-                .child(TextView::new("Keyword:(seperated with ' ', case sensitive)").fixed_width(30))
+                .child(TextView::new("Keyword:(seperated with ' ', case sensitive)").fixed_width(70))
                 .child(TextView::new("you can also separate the context from the actual item using '/', \
                 for example, \"Street Fighter/Ken\" means you want to find content related to the character named Ken in game Street Fighter, \
                 not Ken in any other place. This is useful if you want sentences containing \"Ken\", \
@@ -54,38 +68,47 @@ async fn main() {
                     .unwrap();
                 // use sink to update the UI thread safely
                 let sink = s.cb_sink().clone();
+                // create a clone of the global_string
+                let global_string_clone = Arc::clone(&GLOBAL_STRING);
 
                 // create an async task, as Cursive's event handling is synchronous based
-                tokio::spawn(async move{
+                tokio::spawn(async move {
                     // format the result or handle the error if search fails
                     if let Ok(result_vec) = web::web_main(&keyword[..], &websites[..]).await{
-                        let result_string = format_the_result(result_vec);
                         // update the output box
-                        sink.send(Box::new(|s| {
-                            s.call_on_name("output", |view: &mut TextView| {
-                                view.set_content(result_string);
-                            }).unwrap();
-                            s.add_layer(
-                                Dialog::new()
-                                    .title("Information")
-                                    .content(TextView::new("craw complete!"))
-                                    .button("Ok", |s| {
-                                        //re-enable the EditViews and Buttons
-                                        s.call_on_name("Keyword", |view: &mut EditView| {
-                                            view.set_enabled(true);
-                                        });
-                                        s.call_on_name("Websites", |view: &mut EditView| {
-                                            view.set_enabled(true);
-                                        });
-                                        s.call_on_name("Craw", |view: &mut Button| {
-                                            view.set_enabled(true);
-                                        });
-                                        s.call_on_name("Export", |view: &mut Button| {
-                                            view.set_enabled(true);
-                                        });
-                                        s.pop_layer(); // remove the dialog box
-                                    }),
-                            );
+                        sink.send(Box::new(move |s| {
+                            if let Ok(mut global_string_lock) = global_string_clone.lock(){
+                                *global_string_lock = format_the_result(result_vec);
+                                s.call_on_name("output", |view: &mut TextView| {
+                                    // truncate the string to avoid running out of memory
+                                    if global_string_lock.chars().count() < 1000 {
+                                        view.set_content(global_string_lock.clone());
+                                    }else {
+                                        view.set_content(global_string_lock.chars().take(3000).collect::<String>())
+                                    }
+                                }).unwrap();
+                                s.add_layer(
+                                    Dialog::new()
+                                        .title("Information")
+                                        .content(TextView::new("craw complete!"))
+                                        .button("Ok", |s| {
+                                            //re-enable the EditViews and Buttons
+                                            s.call_on_name("Keyword", |view: &mut EditView| {
+                                                view.set_enabled(true);
+                                            });
+                                            s.call_on_name("Websites", |view: &mut EditView| {
+                                                view.set_enabled(true);
+                                            });
+                                            s.call_on_name("Craw", |view: &mut Button| {
+                                                view.set_enabled(true);
+                                            });
+                                            s.call_on_name("Export", |view: &mut Button| {
+                                                view.set_enabled(true);
+                                            });
+                                            s.pop_layer(); // remove the dialog box
+                                        }),
+                                );
+                            }
                         })).unwrap();
                     }else {
                         // pop a message box to inform the user of an error
@@ -119,7 +142,7 @@ async fn main() {
 
     // the output box with a export button
     let output = LinearLayout::vertical()
-        .child(TextView::new("Found results:"))
+        .child(TextView::new("Found results: (if the result is too long, only the first 3000 characters will be shown)").fixed_width(70))
         .child(
             Panel::new(
                 ScrollView::new(
@@ -142,12 +165,13 @@ async fn main() {
                 view.set_enabled(false);
             });
 
-            //export to a file
+            // export to a file
             if let Ok(mut file) = File::create("craw result.txt"){
-                let output = s
-                    .call_on_name("output", |view: &mut TextView| String::from(view.get_content().source()))
-                    .unwrap();
-                let _ = file.write_all(output.as_bytes());
+                // create a clone of the global_string
+                let global_string_clone = Arc::clone(&GLOBAL_STRING);
+                if let Ok(global_string_lock) = global_string_clone.lock(){
+                    let _ = file.write_all(global_string_lock.as_bytes());
+                };
             }
 
             //show a dialog box
